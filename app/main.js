0f48e4162ed41632cf282633315ec2985b9ce8a3
Brian Nelson <brian.nelson@nisc.coop>
2016-08-04 16:24:50 -0500

    added more features in setting json

--- app/main.js
+++ app/main.js
@@ -11,15 +11,13 @@ const configs = require('./res/modules/config-module.js');
 const settings = require('./res/modules/setting-module.js');
 
 var menubar = require('menubar');
-var path = require('path');
-var cachedBounds; // cachedBounds are needed for double-clicked event
 
 var mb = menubar(configs.getMenubarConfig());
 
 mb.on('ready', function ready() {
     console.log('app is ready');
 
-    util.setupMediaKeyEvents(mb.window);
+    util.setupMediaKeyEvents(mb);
 
     var tray = mb.tray;
     var contextMenu = new Menu();
@@ -29,32 +27,11 @@ mb.on('ready', function ready() {
     tray.setContextMenu(contextMenu);
 });
 
-function watchSettings() {
-    const electronSettings = settings.settings();
-    electronSettings.watch('position', function (data) {
-        mb.positioner.move(data);
-        var position = mb.positioner.calculate(data, cachedBounds);
-        mb.window.setPosition(position.x, position.y);
-    });
-    electronSettings.watch('theme', function (data) {
-        var image = path.join(__dirname, 'res/images', 'gmusic-' + data + '-indicator.png');
-        var tray = mb.tray;
-        tray.setImage(image);
-    });
-}
-mb.on('after-create-window', function() {
-    //mb.window.setResizable(false);
-    //console.log(mb.window.isResizable())
-    watchSettings();
-});
-
 function clicked(e, bounds) {
-    if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey) return mb.hideWindow();
-    if (mb.window && mb.window.isVisible()) return mb.hideWindow();
-    cachedBounds = bounds || cachedBounds;
-    mb.showWindow(cachedBounds);
+    util.showWindow(e, bounds, mb)
 }
 
 ipcMain.on('close-settings', function() {
     util.closeSettings();
+    util.setupMediaKeyEvents(mb);
 });
